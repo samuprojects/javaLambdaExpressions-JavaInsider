6104eafaec07d1d45433c7606a9e05253108ce79
Samuel <samugit.projects@gmail.com>
2023-12-20 13:18:26 -0300

    Aula01 Expressões lambda e a programação funcional

---
+++ src/lambda/theory/HistoryAndConcepts.java
@@ -0,0 +1,24 @@
+package lambda.theory;
+
+@SuppressWarnings("ALL")
+public class HistoryAndConcepts {
+
+    /*
+    * O Java teve vários momentos em sua história onde recursos foram adicionados e mudaram radicalmente a forma de trabalhar com coleções no Java
+    *
+    * Começou no Java 5 com a introdução de Generics
+    *
+    * Em seguida no Java 8 com a introdução das Expressões Lambda
+    *
+    * Seu uso é interessante pois no contexto do Java, que é baseado no paradigma de Orientação a Objetos, permite trabalhar com o paradigma de programação Funcional
+    *
+    * A utilização de Lambda em alguns contextos pode ajudar na economia de diversas linhas de código, poupando tempo e esforço.
+    *
+    * Toda a parte de expressões lambda estão apoiadas sobre um conceito visto no módulo anterior que são as anonymous inner classes, ou seja, podemos dizer que
+    * lambda é uma evolução de inner classes anonimas, o que não significa que uma expressão lambda quando compilada vira uma anonymous inner class.
+    *
+    * Os engenheiros do Java decidiram utilizar um outro mecanismo no bytecode, quando lambdas são compiladas, de forma que dizer que são anonymous inner class é falso.
+    * É gerado um outro mecanismo de classe anônima junto a JVM, mas que conceitualmente se assemelham as inner class anonimas.
+    *
+    * */
+}
